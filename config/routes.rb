a94ce6ee587d8d6c8403ff94402818961d5a5808
wc.kamata <wc_kamata05@yahoo.co.jp>
2020-01-28 14:37:10 +0000

    kadai-tasklist

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'tasks#index'
+
+  #CRUD + index: show の補助ページ* + new: 新規作成用のフォームページ + edit: 更新用のフォームページ
+  resources :tasks
 end
