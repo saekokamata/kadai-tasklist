5815a6e956f2ce385506ee6b0282ef7d78894cc9
wc.kamata <wc_kamata05@yahoo.co.jp>
2020-02-09 06:18:54 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  #get 'users/index'
+  #get 'users/show'
+  #get 'users/new'
+  #get 'users/create'
   root to: 'tasks#index'
 
   #CRUD + index: show の補助ページ* + new: 新規作成用のフォームページ + edit: 更新用のフォームページ
-  resources :tasks
+  #resources :tasks
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
+  
 end
