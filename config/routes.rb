4291bd2dff18c2e4a8b8102dcc69847e8f8ee699
wc.kamata <wc_kamata05@yahoo.co.jp>
2020-02-09 06:52:50 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   #get 'users/index'
   #get 'users/show'
   #get 'users/new'
   #get 'users/create'
   root to: 'tasks#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   #CRUD + index: show の補助ページ* + new: 新規作成用のフォームページ + edit: 更新用のフォームページ
   #resources :tasks
